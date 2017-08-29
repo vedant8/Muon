326e31ee828b64491115f29a38d4e39cac7108dd
Virul <22166196+Virul-git@users.noreply.github.com>
2017-08-29 19:51:02 +0530

    Add files via upload
    changes to match the vhdl syntax

--- Python_Scripts/generate.py
+++ Python_Scripts/generate.py
@@ -9,26 +9,26 @@ n=8
 def left_bound(a):
 	print "left_bound"+str(a)+"<=",
 	for j in range(a,n-a-1):
-		print "sample_array("+str(a)+','+str(j)+")"+"+",
-	print "sample_array("+str(a)+','+str(n-a-1)+")"
+		print "sample_array("+str(a)+','+str(j)+")"+" or",
+	print "sample_array("+str(a)+','+str(n-a-1)+");"
 
 def right_bound(a):
 	print "right_bound"+str(a)+"<=",
 	for j in range(a,n-a-1):
-		print "sample_array("+str(n-a-1)+','+str(j)+")"+"+",
-	print "sample_array("+str(n-a-1)+','+str(n-a-1)+")"
+		print "sample_array("+str(n-a-1)+','+str(j)+")"+" or",
+	print "sample_array("+str(n-a-1)+','+str(n-a-1)+");"
 
 def lower_bound(a):
 	print "lower_bound"+str(a)+"<=",
 	for j in range(a,n-a-1):
-		print "sample_array("+str(j)+','+str(a)+")"+"+",
-	print "sample_array("+str(n-a-1)+','+str(a)+")"
+		print "sample_array("+str(j)+','+str(a)+")"+" or",
+	print "sample_array("+str(n-a-1)+','+str(a)+");"
 
 def up_bound(a):
 	print "up_bound"+str(a)+"<=",
 	for j in range(a,n-a-1):
-		print "sample_array("+str(j)+','+str(n-a-1)+")"+"+",
-	print "sample_array("+str(n-a-1)+','+str(n-a-1)+")"
+		print "sample_array("+str(j)+','+str(n-a-1)+")"+" or",
+	print "sample_array("+str(n-a-1)+','+str(n-a-1)+");"
 
 for j in range(n//2):
 	left_bound(j)
