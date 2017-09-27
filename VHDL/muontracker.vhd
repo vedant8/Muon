046192ffc5bcf214896c181c82759d78b4f4d52f
Virul <22166196+Virul-git@users.noreply.github.com>
2017-09-27 21:14:25 +0530

    Direction finding array updating

--- VHDL/muontracker.vhd
+++ VHDL/muontracker.vhd
@@ -20,10 +20,36 @@ signal ctr_left:integer:=0;
 signal ctr_right:integer:=0;
 signal ctr_upper:integer:=0;
 signal ctr_lower:integer:=0;
+signal hit:std_logic:='0';
+signal track_flag_1:integer:=0;
 signal hit_array_x:arr_type_2;
 signal hit_array_y:arr_type_2;
+signal path_dir_array:arr_type_2;
+signal path_dir_array2:integer:=0;
 signal sample_array: arr_type_1;
-signal hit:std_logic:='0';
+signal x_n:integer:=0;
+signal y_n:integer:=0;
+signal x_n1:integer:=0;
+signal y_n1:integer:=0;
+signal n:integer:=0;
+
+signal path_dir_array_d0:integer:=0;
+signal path_dir_array_d1:integer:=0;
+signal path_dir_array_d2:integer:=0;
+signal path_dir_array_d3:integer:=0;
+signal path_dir_array_d4:integer:=0;
+signal path_dir_array_d5:integer:=0;
+signal path_dir_array_d6:integer:=0;
+signal path_dir_array_d7:integer:=0;
+signal path_dir_array_d8:integer:=0;
+signal path_dir_array_d9:integer:=0;
+signal path_dir_array_d10:integer:=0;
+signal path_dir_array_d11:integer:=0;
+signal path_dir_array_d12:integer:=0;
+signal path_dir_array_d13:integer:=0;
+signal path_dir_array_d14:integer:=0;
+
+
 
 	signal count : integer := 0;
 	signal count_old : integer := 0;
@@ -34,13 +60,14 @@ signal hit:std_logic:='0';
 	--variable v : integer := hit_array_y(0);
 begin
 --populating the matrix
+
 sample_array(0,0)<='0';
 sample_array(0,1)<='0';
 sample_array(0,2)<='0';
 sample_array(0,3)<='0';
 sample_array(0,4)<='0';
 sample_array(0,5)<='0';
-sample_array(0,6)<='1';
+sample_array(0,6)<='0';
 sample_array(0,7)<='0';
 	sample_array(1,0)<='0';
 	sample_array(1,1)<='1';
@@ -109,42 +136,196 @@ lower_bound<= sample_array(0,0) or sample_array(1,0) or sample_array(2,0) or sam
 
 up_bound<= sample_array(7,7) or sample_array(6,7) or sample_array(5,7) or sample_array(4,7) or sample_array(3,7) or sample_array(2,7) or sample_array(1,7) or sample_array(0,7);
 --first hit on left border
+path_dir_array2 <= path_dir_array(6);
 process(clk)
 begin
+path_dir_array_d0 <= path_dir_array(0);
+path_dir_array_d1 <= path_dir_array(1);
+path_dir_array_d2 <= path_dir_array(2);
+path_dir_array_d3 <= path_dir_array(3);
+path_dir_array_d4 <= path_dir_array(4);
+path_dir_array_d5 <= path_dir_array(5);
+path_dir_array_d6 <= path_dir_array(6);
+path_dir_array_d7 <= path_dir_array(7);
+path_dir_array_d8 <= path_dir_array(8);
+path_dir_array_d9 <= path_dir_array(9);
+path_dir_array_d10 <= path_dir_array(10);
+path_dir_array_d11 <= path_dir_array(11);
+path_dir_array_d12 <= path_dir_array(12);
+path_dir_array_d13 <= path_dir_array(13);
+path_dir_array_d14 <= path_dir_array(14);
+
 if hit='0' then
 	if left_bound='1' then
 		if sample_array(0,ctr_left) = '1' then
-			hit_array_x(0)<=0;
-			hit_array_y(0)<=ctr_left;
+			x_n<=0;
+			y_n<=ctr_left;
 			hit<='1';
 		end if;
 		ctr_left<=ctr_left+1;
 
 	elsif right_bound='1' then
 		if sample_array(7,ctr_right) = '1' then
-			hit_array_x(0)<=7;
-			hit_array_y(0)<=ctr_right;
+			x_n<=7;
+			y_n<=ctr_right;
 			hit<='1';
 		end if;
 		ctr_right<=ctr_right+1;
 
 	elsif up_bound='1' then
 		if sample_array(ctr_upper,7) = '1' then
-			hit_array_y(0)<=7;
-			hit_array_x(0)<=ctr_upper;
+			y_n<=7;
+			x_n<=ctr_upper;
 			hit<='1';
 		end if;
 		ctr_upper<=ctr_upper+1;
 
 	elsif lower_bound='1' then
 		if sample_array(ctr_lower,0) = '1' then
-			hit_array_y(0)<=0;
-			hit_array_x(0)<=ctr_lower;
+			y_n<=0;
+			x_n<=ctr_lower;
 			hit<='1';
 		end if;
 		ctr_lower<=ctr_lower+1;	
 end if;
 end if;	
 end process;
+process(clk)
+begin 
+hit_array_x(0) <= x_n;
+hit_array_y(0) <= y_n;
+ 
 
+if hit='1' and track_flag_1=0 then
+	  if x_n=0 then
+		if y_n=0 then
+			 if sample_array(1,0)= '1' then
+			 	x_n1<=1;
+			 	y_n1<=0;
+			 	path_dir_array(0)<=0;
+			 	track_flag_1<=1;
+			 elsif	sample_array(x_n+1,y_n+1)= '1' then
+			 	x_n1<=1;
+			 	y_n1<=1;
+			 	path_dir_array(0)<=1;
+			 	track_flag_1<=1;
+			 elsif sample_array(0,1)= '1' then
+			 	x_n1<=0;
+			 	y_n1<=1;
+			 	path_dir_array(0)<=2;
+			 	track_flag_1<=1;
+			 end if;	
+		elsif y_n=7 then
+			 if sample_array(0,6)= '1' then
+			 	x_n1<=0;
+			 	y_n1<=6;
+			 	path_dir_array(0)<=3;
+			 	track_flag_1<=1;
+			 elsif	sample_array(1,6)= '1' then
+			 	x_n1<=1;
+			 	y_n1<=6;
+			 	path_dir_array(0)<=4;
+			 	track_flag_1<=1;
+			 elsif sample_array(1,7)= '1' then
+			 	x_n1<=1;
+			 	y_n1<=7;
+			 	path_dir_array(0)<=0;
+			 	track_flag_1<=1;
+			 end if	;
+		else 
+			 if sample_array(0,hit_array_y(0)+1)='1' then
+			  x_n1<=0;
+			  y_n1<=hit_array_y(0)+1;
+			  path_dir_array(0)<=2;
+			  track_flag_1<=1;
+			 elsif sample_array(1,hit_array_y(0)+1)='1' then
+			  x_n1<=1;
+			  y_n1<=hit_array_y(0)+1;
+			  path_dir_array(0)<=1;
+			  track_flag_1<=1;
+			 elsif sample_array(1,hit_array_y(0))='1' then
+			  x_n1<=1;
+			  y_n1<=hit_array_y(0);
+			  path_dir_array(0)<=0; 
+			  track_flag_1<=1;
+			 elsif sample_array(1,hit_array_y(0)-1)='1' then
+			  x_n1<=1;
+			  y_n1<=hit_array_y(0)-1;
+			  track_flag_1<=1;
+			  path_dir_array(0)<=4;
+			 elsif sample_array(0,hit_array_y(0)-1)='1' then
+			  x_n1<=0;
+			  y_n1<=hit_array_y(0)-1;
+			  path_dir_array(0)<=3;
+			  track_flag_1<=1;
+			 end if;
+		end if;
+	  elsif	 x_n=7 then  
+		    if y_n=0 then
+				 if sample_array(6,0)= '1' then
+				 	x_n1<=6;
+				 	y_n1<=0;
+				 	path_dir_array(0)<=6;
+				 	track_flag_1<=1;
+				 elsif	sample_array(6,1)= '1' then
+				 	x_n1<=6;
+				 	y_n1<=1;
+				 	path_dir_array(0)<=7;
+				 	track_flag_1<=1;
+				 elsif sample_array(7,1)= '1' then
+				 	x_n1<=7;
+				 	y_n1<=1;
+				 	path_dir_array(0)<=2;
+				 	track_flag_1<=1;
+				 end if;	
+		    elsif y_n=7 then
+				 if sample_array(7,6)= '1' then
+				 	x_n1<=7;
+				 	y_n1<=6;
+				 	path_dir_array(0)<=3;
+				 	track_flag_1<=1;
+				 elsif	sample_array(6,6)= '1' then
+				 	x_n1<=6;
+				 	y_n1<=6;
+				 	path_dir_array(0)<=5;
+				 	track_flag_1<=1;
+				 elsif sample_array(6,7)= '1' then
+				 	x_n1<=6;
+				 	y_n1<=7;
+				 	path_dir_array(0)<=6;
+				 	track_flag_1<=1;
+				 end if	;
+		     else
+				 if sample_array(7,hit_array_y(0)+1)='1' then
+				  x_n1<=7;
+				  y_n1<=hit_array_y(0)+1;
+				  path_dir_array(0)<=2;
+				  track_flag_1<=1;
+				 elsif sample_array(6,hit_array_y(0)+1)='1' then
+				  x_n1<=6;
+				  y_n1<=hit_array_y(0)+1;
+				  path_dir_array(0)<=7;
+				  track_flag_1<=1;
+				 elsif sample_array(6,hit_array_y(0))='1' then
+				  x_n1<=6;
+				  y_n1<=hit_array_y(0);
+				  path_dir_array(0)<=6; 
+				  track_flag_1<=1;
+				 elsif sample_array(6,hit_array_y(0)-1)='1' then
+				  x_n1<=6;
+				  y_n1<=hit_array_y(0)-1;
+				  path_dir_array(0)<=5;
+				  track_flag_1<=1;
+				 elsif sample_array(7,hit_array_y(0)-1)='1' then
+				  x_n1<=0;
+				  y_n1<=hit_array_y(0)-1;
+				  path_dir_array(0)<=3;
+				  track_flag_1<=1;
+				 end if;
+			end if;  				
+	end if;
+end if;
+
+end process;
 end Behavioral;
+
